da1d157a0f3321b22f1a678bf7365f4e6f2c9eac
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-02 15:09:03 +0900

    Add number as token

--- lib/tokenizer.ml
+++ lib/tokenizer.ml
@@ -1,16 +1,20 @@
-let print_tokens tokens = print_endline (String.concat " " tokens)
+let print_tokens tokens = print_endline (String.concat "," tokens)
 
 let%expect_test "print_tokens 1 tag" =
   print_tokens [ "<"; "div"; ">"; "a"; "<"; "/"; "div"; ">" ];
-  [%expect {| < div > a < / div > |}]
+  [%expect {| <,div,>,a,<,/,div,> |}]
 
 let get_text_value str =
-  let text_regexp = Str.regexp "[A-Za-z]+" in
+  let text_regexp = Str.regexp "[A-Za-z0-9]+" in
   if Str.string_match text_regexp str 0 then Str.matched_string str
   else failwith (Printf.sprintf "Unknown character `%c`." str.[0])
 
 let%test "get_text_value \"div>\"" = get_text_value "div>" = "div"
 let%test "get_text_value \"div\"" = get_text_value "div" = "div"
+
+let%expect_test "get_text_value with number" =
+  get_text_value "55go</div>" |> print_endline;
+  [%expect {| 55go |}]
 
 let tokenize input_str =
   let rec acc chars =
@@ -36,6 +40,10 @@ let tokenize input_str =
 
 let%test "tokenize <div>a</div>" =
   tokenize "<div>a</div>" = [ "<"; "div"; ">"; "a"; "<"; "/"; "div"; ">" ]
+
+let%expect_test "tokenize tag with number text" =
+  tokenize "<div>bob2</div>" |> print_tokens;
+  [%expect {| <,div,>,bob2,<,/,div,> |}]
 
 let%test "tokenize <div class=\"container\">a</div>" =
   tokenize "<div class=\"container\">a</div>"
