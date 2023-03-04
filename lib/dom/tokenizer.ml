229f0b7f854c17a0cba2890f713def583daba0fa
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-04 22:54:09 +0900

    Refactoring DOM tokenizer

--- lib/dom/tokenizer.ml
+++ lib/dom/tokenizer.ml
@@ -1,20 +1,25 @@
-let print_tokens tokens = print_endline (String.concat "," tokens)
+exception Unknown_character of string
 
-let%expect_test "print_tokens 1 tag" =
-  print_tokens [ "<"; "div"; ">"; "a"; "<"; "/"; "div"; ">" ];
-  [%expect {| <,div,>,a,<,/,div,> |}]
+let print_tokens ?(separator = ",") tokens =
+  print_endline (String.concat separator tokens)
 
-let get_text_value str =
-  let text_regexp = Str.regexp "[A-Za-z0-9]+" in
-  if Str.string_match text_regexp str 0 then Str.matched_string str
-  else failwith (Printf.sprintf "Unknown character `%c`." str.[0])
+let%expect_test "print_tokens with '|' separator" =
+  print_tokens [ "<"; "div"; ">"; "a"; "<"; "/"; "div"; ">" ] ~separator:"|";
+  [%expect {| <|div|>|a|<|/|div|> |}]
 
-let%test "get_text_value \"div>\"" = get_text_value "div>" = "div"
-let%test "get_text_value \"div\"" = get_text_value "div" = "div"
-
-let%expect_test "get_text_value with number" =
-  get_text_value "55go</div>" |> print_endline;
-  [%expect {| 55go |}]
+let tokenize_chunk chars =
+  let chunk_regexp = Str.regexp "[A-Za-z0-9-]+" in
+  let input_string =
+    chars |> List.map Base.String.of_char |> String.concat ""
+  in
+  let chunk =
+    if Str.string_match chunk_regexp input_string 0 then
+      Str.matched_string input_string
+    else raise (Unknown_character (Base.String.of_char input_string.[0]))
+  in
+  let pos = String.length chunk in
+  let _, rest = Base.List.split_n chars pos in
+  (chunk, rest)
 
 let tokenize input_str =
   let rec acc tokens chars =
@@ -25,38 +30,28 @@ let tokenize input_str =
         let token = List.hd chars |> Base.String.of_char in
         acc (tokens @ [ token ]) rest
     | _ ->
-        let sub_string =
-          chars |> List.map Base.String.of_char |> String.concat ""
-        in
-        let token = get_text_value sub_string in
-        let pos = String.length token in
-        let _, rest = Base.List.split_n chars pos in
-        acc (tokens @ [ token ]) rest
+        let chunk, rest = tokenize_chunk chars in
+        acc (tokens @ [ chunk ]) rest
   in
   let tokens, _ = acc [] (Base.String.to_list input_str) in
   tokens
 
-let%test "tokenize <div>a</div>" =
-  tokenize "<div>a</div>" = [ "<"; "div"; ">"; "a"; "<"; "/"; "div"; ">" ]
+let%expect_test "tokenize a tag" =
+  "<div>hello</div>" |> tokenize |> print_tokens;
+  [%expect {| <,div,>,hello,<,/,div,> |}]
 
-let%expect_test "tokenize tag with number text" =
-  tokenize "<div>bob2</div>" |> print_tokens;
+let%expect_test "tokenize a tag with number text" =
+  "<div>bob2</div>" |> tokenize |> print_tokens;
   [%expect {| <,div,>,bob2,<,/,div,> |}]
 
-let%test "tokenize <div class=\"container\">a</div>" =
-  tokenize "<div class=\"container\">a</div>"
-  = [
-      "<";
-      "div";
-      "class";
-      "=";
-      "\"";
-      "container";
-      "\"";
-      ">";
-      "a";
-      "<";
-      "/";
-      "div";
-      ">";
-    ]
+let%expect_test "tokenize a tag containing a hyphen in the name" =
+  "<my-element></my-element>" |> tokenize |> print_tokens;
+  [%expect {| <,my-element,>,<,/,my-element,> |}]
+
+let%expect_test "tokenize a tag that has a class attribute" =
+  "<div class=\"container\">hello</div>" |> tokenize |> print_tokens;
+  [%expect {| <,div,class,=,",container,",>,hello,<,/,div,> |}]
+
+let%expect_test "tokenize a tag that has children" =
+  "<ul>\n<li>alice</li>\n<li>bob</li></ul>" |> tokenize |> print_tokens;
+  [%expect {| <,ul,>,<,li,>,alice,<,/,li,>,<,li,>,bob,<,/,li,>,<,/,ul,> |}]
