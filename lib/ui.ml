5c438fc583c0eb241fcac1258c9812065fa789ef
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-04 22:37:05 +0900

    Play with Bogue

--- lib/ui.ml
+++ lib/ui.ml
@@ -1,4 +1,15 @@
 open Bogue
 
+let main () =
+  let b = Widget.check_box () in
+  let l = Widget.label "Hello world" in
+  let style = Style.Solid (Draw.black |> Draw.opaque) |> Style.of_bg in
+  let box = Widget.box ~w:200 ~h:100 ~style () in
+  let layout = Layout.flat_of_w [ b; l; box ] in
+
+  let board = Bogue.of_layout layout in
+  Bogue.run board
+
 let render () =
-  Widget.label "Hello world" |> Layout.resident |> Bogue.of_layout |> Bogue.run
+  main ();
+  Bogue.quit ()
