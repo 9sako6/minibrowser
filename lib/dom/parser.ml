3369597129ab72827638ee9634ad1adc62fa7b11
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-04 23:42:02 +0900

    Refactoring

--- lib/dom/parser.ml
+++ lib/dom/parser.ml
@@ -2,106 +2,83 @@ open Tokenizer
 open Node
 
 exception NoEndTag
+exception Unexpected
 
-let children_and_rest_tokans tokens =
+let omit_end_tag_tokens tokens =
+  match Base.List.rev tokens with
+  | ">" :: _ :: "/" :: "<" :: rest -> Base.List.rev rest
+  | _ -> raise NoEndTag
+
+let%expect_test "omit_end_tag_tokens" =
+  "<li>bob</li></div>" |> tokenize |> omit_end_tag_tokens |> print_tokens;
+  [%expect {| <,li,>,bob,<,/,li,> |}]
+
+let parse_children_tokens tokens =
+  (* When the score is positive, some tag is open. *)
   let rec split score children tails =
     match tails with
     | [] -> raise NoEndTag
+    (* Each time a tag is closed, the score is decremented. *)
     | "<" :: "/" :: rest -> split (score - 1) (children @ [ "<"; "/" ]) rest
+    (* Each time a tag is opened, the score is incremented. *)
     | "<" :: rest -> split (score + 1) (children @ [ "<" ]) rest
     | head :: rest ->
         if score = 0 then
           match head with
-          | ">" -> (children @ [ ">" ], rest)
+          | ">" ->
+              (* Omit parent's end tag. *)
+              let children_tokens = children @ [ ">" ] |> omit_end_tag_tokens in
+              (children_tokens, rest)
           | _ -> split 0 (children @ [ head ]) rest
         else if score > 0 then split score (children @ [ head ]) rest
-        else failwith "Fail to parse."
+        else raise Unexpected
   in
-  let children, rest = split 1 [] tokens in
-  let children =
-    match Base.List.rev children with
-    | ">" :: _ :: "/" :: "<" :: tails -> Base.List.rev tails
-    | _ -> raise NoEndTag
+  let children_tokens, rest = split 1 [] tokens in
+  (children_tokens, rest)
+
+let%expect_test "parse_children_tokens" =
+  let children_tokens, rest =
+    "<p>child1</p><p>child2</p></p><p>rest</p>" |> tokenize
+    |> parse_children_tokens
   in
-  (children, rest)
+  children_tokens |> print_tokens;
+  rest |> print_tokens;
+  [%expect
+    {|
+    <,p,>,child1,<,/,p,>,<,p,>,child2,<,/,p,>
+    <,p,>,rest,<,/,p,>
+  |}]
 
-let%test "split_children_and_rest <p>child1</p><p>child2</p></p><p>rest</p>" =
-  children_and_rest_tokans
-    [
-      (* <p>child1</p><p>child2</p> *)
-      "<";
-      "p";
-      ">";
-      "child1";
-      "<";
-      "/";
-      "p";
-      ">";
-      "<";
-      "p";
-      ">";
-      "child2";
-      "<";
-      "/";
-      "p";
-      ">";
-      "<";
-      "/";
-      "p";
-      ">";
-      (* <p>rest</p> *)
-      "<";
-      "p";
-      ">";
-      "rest";
-      "<";
-      "/";
-      "p";
-      ">";
-    ]
-  = ( [
-        "<";
-        "p";
-        ">";
-        "child1";
-        "<";
-        "/";
-        "p";
-        ">";
-        "<";
-        "p";
-        ">";
-        "child2";
-        "<";
-        "/";
-        "p";
-        ">";
-      ],
-      [ "<"; "p"; ">"; "rest"; "<"; "/"; "p"; ">" ] )
-
-let attributes_and_rest_tokens tokens =
-  let rec split attributes rest =
+let parse_attributes tokens =
+  let rec acc attributes rest =
     match rest with
     | [] -> (attributes, [])
     | ">" :: rest -> (attributes, rest)
-    | attribute_name :: "=" :: "\"" :: attribute_value :: "\"" :: rest ->
-        let attribute = Attribute.create attribute_name attribute_value in
-        split (attributes @ [ attribute ]) rest
+    | name :: "=" :: "\"" :: value :: "\"" :: rest ->
+        let attribute = Attribute.create name value in
+        acc (attributes @ [ attribute ]) rest
     | _ -> ([], rest)
   in
-  split [] tokens
+  acc [] tokens
 
 let rec parse tokens =
-  match tokens with
-  | [] -> []
-  (* Start of tag *)
-  | "<" :: tag_name :: rest ->
-      let attributes, rest = attributes_and_rest_tokens rest in
-      let children, rest = children_and_rest_tokans rest in
-      Element (tag_name, attributes, parse children) :: parse rest
-  (* End of tag *)
-  | text :: "<" :: "/" :: _name :: ">" :: rest -> InnerText text :: parse rest
-  | text :: rest -> InnerText text :: parse rest
+  let rec acc nodes rest =
+    match rest with
+    | [] -> (nodes, [])
+    (* Start of a tag *)
+    | "<" :: tag_name :: rest ->
+        let attributes, rest = parse_attributes rest in
+        let children_tokens, rest = parse_children_tokens rest in
+        acc
+          (nodes @ [ Element (tag_name, attributes, parse children_tokens) ])
+          rest
+    (* End of a tag *)
+    | text :: "<" :: "/" :: _ :: ">" :: rest
+    (* Inner text *)
+    | text :: rest -> acc (nodes @ [ InnerText text ]) rest
+  in
+  let nodes, _ = acc [] tokens in
+  nodes
 
 let%test "parse 1 tag" =
   tokenize "<div>alice</div>"
