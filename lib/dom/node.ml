02e3e42bf06618426756948ec978d3c9a78df064
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-04 14:22:20 +0900

    Add matches

--- lib/dom/node.ml
+++ lib/dom/node.ml
@@ -2,11 +2,19 @@ module Attribute : sig
   type t
 
   val create : string -> string -> t
+  val name : t -> string
+  val value : t -> string
   val to_string : t -> string
 end = struct
   type t = string * string
 
   let create key value = (key, value)
+
+  let name = function
+    | name, _ -> name
+
+  let value = function
+    | _, value -> value
 
   let to_string attribute =
     match attribute with
--- lib/style.ml
+++ lib/style.ml
@@ -1,1 +1,57 @@
-type t = Style_node of Dom.Node.t * Css.Node.rule * t list
+type t = Style of Dom.Node.t * Css.Node.declaration list * t list
+
+(* any of the attributes match the selectors *)
+let matches dom_node rule =
+  let attributes =
+    match dom_node with
+    | Dom.Node.Element (_, attributes, _) -> attributes
+    | Dom.Node.InnerText _ -> []
+  in
+  let selectors =
+    match rule with
+    | Css.Node.Rule (selectors, _) -> selectors
+  in
+  let matches_with_selector attributes selector =
+    match selector with
+    | Css.Node.Universal_selector -> true
+    | Css.Node.Class_selector class_name ->
+        let acc attribute =
+          let attribute_name = Dom.Node.Attribute.name attribute in
+          let value = Dom.Node.Attribute.value attribute in
+          attribute_name = "class" && value = class_name
+        in
+        List.exists acc attributes
+  in
+  List.exists (matches_with_selector attributes) selectors
+
+let%expect_test "matches" =
+  let rule =
+    Css.Node.Rule
+      ([ Css.Node.Class_selector "alert"; Css.Node.Universal_selector ], [])
+  in
+  let dom_node =
+    "<div class=\"alert\" id=\"foo\">alert</div>" |> Dom.Tokenizer.tokenize
+    |> Dom.Parser.parse |> List.hd
+  in
+  matches dom_node rule |> string_of_bool |> print_endline;
+  [%expect {| true |}]
+
+let%expect_test "matches" =
+  let rule = Css.Node.Rule ([ Css.Node.Universal_selector ], []) in
+  let dom_node =
+    "<div id=\"foo\">alert</div>" |> Dom.Tokenizer.tokenize |> Dom.Parser.parse
+    |> List.hd
+  in
+  matches dom_node rule |> string_of_bool |> print_endline;
+  [%expect {| true |}]
+
+let%expect_test "matches" =
+  let rule = Css.Node.Rule ([ Css.Node.Class_selector "alert" ], []) in
+  let dom_node =
+    "<div id=\"foo\">alert</div>" |> Dom.Tokenizer.tokenize |> Dom.Parser.parse
+    |> List.hd
+  in
+  matches dom_node rule |> string_of_bool |> print_endline;
+  [%expect {| false |}]
+
+let create dom_node _stylesheet = Style (dom_node, [], [])
