9fe184c042c02b92ff7d92f25c5627a42a2ce851
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-08 19:39:32 +0900

    Fix tokenizer

--- lib/dom/tokenizer.ml
+++ lib/dom/tokenizer.ml
@@ -12,9 +12,9 @@ let matched_string regexp string =
   else Not_matched string |> raise
 
 let tokenize_text chars =
-  let text_regexp = Str.regexp "[^<]+" in
+  let regexp = Str.regexp "[^<]+" in
   let input_string = Base.String.of_char_list chars in
-  let chunk = matched_string text_regexp input_string in
+  let chunk = matched_string regexp input_string in
   let rest =
     Base.String.drop_prefix input_string (String.length chunk)
     |> Base.String.to_list
@@ -32,10 +32,32 @@ let%expect_test "tokenize_text" =
     </div>
   |}]
 
-let tokenize_chunk chars =
-  let chunk_regexp = Str.regexp "[A-Za-z0-9-.]+" in
+let tokenize_attribute_value chars =
+  let regexp = Str.regexp "[^\"]+" in
   let input_string = Base.String.of_char_list chars in
-  let chunk = matched_string chunk_regexp input_string in
+  let chunk = matched_string regexp input_string in
+  let rest =
+    Base.String.drop_prefix input_string (String.length chunk)
+    |> Base.String.to_list
+  in
+  (chunk, rest)
+
+let%expect_test "tokenize_attribute_value" =
+  let text, rest =
+    "alert\">Hello, World!</div>" |> Base.String.to_list
+    |> tokenize_attribute_value
+  in
+  print_endline text;
+  List.iter print_char rest;
+  [%expect {|
+    alert
+    ">Hello, World!</div>
+  |}]
+
+let tokenize_chunk chars =
+  let regexp = Str.regexp "[A-Za-z0-9-]+" in
+  let input_string = Base.String.of_char_list chars in
+  let chunk = matched_string regexp input_string in
   let rest =
     Base.String.drop_prefix input_string (String.length chunk)
     |> Base.String.to_list
@@ -54,16 +76,22 @@ let%expect_test "tokenize_chunk" =
   |}]
 
 let tokenize input_string =
-  let rec aux ?(is_in_tag = false) tokens chars =
+  let rec aux ?(is_in_tag = false) ?(is_in_attribute = false) tokens chars =
     match chars with
     | [] -> (tokens, [])
     | (' ' | '\n') :: rest -> aux tokens rest
-    | (('<' | '/' | '=' | '"') as head) :: rest ->
+    | (('<' | '/' | '=') as head) :: rest ->
         aux (tokens @ [ Base.String.of_char head ]) rest ~is_in_tag:false
+    | '"' :: rest ->
+        aux (tokens @ [ "\"" ]) rest ~is_in_tag:false
+          ~is_in_attribute:(not is_in_attribute)
     | '>' :: rest -> aux (tokens @ [ ">" ]) rest ~is_in_tag:true
     | _ ->
         let chunk, rest =
-          if is_in_tag then tokenize_text chars else tokenize_chunk chars
+          match (is_in_tag, is_in_attribute) with
+          | true, _ -> tokenize_text chars
+          | false, true -> tokenize_attribute_value chars
+          | false, false -> tokenize_chunk chars
         in
         aux (tokens @ [ chunk ]) rest
   in
