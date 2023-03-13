c3ef32675a9e51cb8a1fe280f5c8cc9c6aa06c5b
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-13 22:43:45 +0900

    Refactoring

--- lib/css/value_map.ml
+++ lib/css/value_map.ml
@@ -1,19 +1,9 @@
-module Value_map = Map.Make (String)
-include Value_map
+module M = Map.Make (String)
+include M
 
-type t = Value.t Value_map.t
+type t = Value.t M.t
 
-let pp map =
-  let bindings = Value_map.bindings map in
-  let rec aux bindings acc =
-    match bindings with
-    | [] -> (acc, [])
-    | (name, value) :: rest ->
-        let string = Printf.sprintf "%s: %s;" name (Value.show value) in
-        aux rest (acc @ [ string ])
-  in
-  let strings, _ = aux bindings [] in
-  String.concat " " strings
+let pp map = [%show: (string * Value.t) list] (M.bindings map)
 
 let rec lookup keys default_value map =
   match keys with
