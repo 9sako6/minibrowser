f3436c9b7e8ebfce71428c19b02da763b1c970b6
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-15 23:17:58 +0900

    Add comments

--- lib/style.mli
+++ lib/style.mli
@@ -1,10 +1,12 @@
+(* A styled DOM node with calculated size properties *)
 type t = {
-  node : Dom.Node.t ref;
-  specified_values : Css.Value_map.t;
-  children : t list;
-  size : size;
+  node : Dom.Node.t ref; (* Reference to the corresponding DOM node *)
+  specified_values : Css.Value_map.t; (* CSS property-value pairs *)
+  children : t list; (* List of styled child nodes *)
+  size : size; (* Calculated size properties *)
 }
 
+(* Size properties for a styled DOM node *)
 and size = {
   width : px;
   height : px;
@@ -25,13 +27,25 @@ and size = {
   margin_left : px;
 }
 
+(* A size value in pixels or 'auto' *)
 and px =
   | Px of float
   | Auto
 
+(* Returns an empty styled node *)
 val empty : unit -> t
+
+(* Converts a styled node to a string with optional indentation *)
 val to_string : ?indent:string -> t -> string
+
+(* Builds a list of styled nodes from HTML and CSS strings *)
 val build_styles : html:string -> css:string -> t list
+
+(* Returns the background color of a styled node as an (r, g, b) tuple *)
 val get_background_color : t -> int * int * int
+
+(* Returns the float value of a size property, treating 'Auto' as 0.0 *)
 val get_size_value : px -> float
+
+(* Adds two size values, treating 'Auto' as 0.0 *)
 val ( + ) : px -> px -> px
