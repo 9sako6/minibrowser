acdb9258824bbf55cc6664ade0cb664c598fae5b
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-03-16 00:56:46 +0900

    Refactoring

--- lib/gui.ml
+++ lib/gui.ml
@@ -1,28 +1,20 @@
-let draw_rect rect context =
-  match rect with
-  | Display_command.Rect ((r, g, b), { x; y; width; height }) ->
-      let x0 = x in
-      let x1 = x +. width in
-      let y0 = y in
-      let y1 = y +. height in
-      Cairo.set_source_rgba context 0. 0. 0. 1.;
-      Cairo.move_to context x0 y0;
-      List.iter
-        (fun (x, y) -> Cairo.line_to context x y)
-        [ (x1, y0); (x1, y1); (x0, y1) ];
-      Cairo.set_source_rgba context r g b 1.;
-      Cairo.fill context
+let draw_rect (Display_command.Rect ((r, g, b), { x; y; width; height }))
+    context =
+  let x0 = x in
+  let x1 = x +. width in
+  let y0 = y in
+  let y1 = y +. height in
+  Cairo.set_source_rgba context 0. 0. 0. 1.;
+  Cairo.move_to context x0 y0;
+  List.iter
+    (fun (x, y) -> Cairo.line_to context x y)
+    [ (x1, y0); (x1, y1); (x0, y1) ];
+  Cairo.set_source_rgba context r g b 1.;
+  Cairo.fill context
 
 let draw _drawing_area ~max_width ~html ~css context =
   let commands = Display_command.build ~max_width ~html ~css in
-  let rec aux context commands =
-    match commands with
-    | [] -> true
-    | (Display_command.Rect _ as rect) :: rest ->
-        draw_rect rect context;
-        aux context rest
-  in
-  ignore (aux context commands);
+  List.iter (fun cmd -> draw_rect cmd context) commands;
   true
 
 let render_on_window ~html ~css ~window_width ~window_height () =
